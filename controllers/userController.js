3a1bf4053820bebcbcad11eaa632cf3853bd389d
soniagkalliath <soniageorge111@gmail.com>
2025-12-09 13:05:21 +0530

    ADD : Google login

--- controllers/userController.js
+++ controllers/userController.js
@@ -43,5 +43,30 @@ exports.loginController = async (req,res)=>{
         res.status(500).json(error)
     }
 }
+
+//google login
+exports.googleLoginController = async (req,res)=>{
+    console.log("Inside googleLoginController");
+    const {email,password,username,picture} = req.body
+    console.log(email,password,username,picture);    
+    try{
+        const existingUser = await users.findOne({email})
+        if(existingUser){
+            //login
+            const token = jwt.sign({userMail:existingUser.email,role:existingUser.role},process.env.JWTSECRET)
+            res.status(200).json({user:existingUser,token})
+        }else{
+            //register
+            const newUser = await users.create({
+                username,email,password,picture
+            })
+            const token = jwt.sign({userMail:newUser.email,role:newUser.role},process.env.JWTSECRET)
+            res.status(200).json({user:newUser,token})
+        }
+    }catch(error){
+        console.log(error);        
+        res.status(500).json(error)
+    }
+}
 //user profile edit
 //admin profile edit
