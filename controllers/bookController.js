2a48f093581275798820c49916ce5e7fbd1cefb9
soniagkalliath <soniageorge111@gmail.com>
2025-12-17 15:32:14 +0530

    ADD : logic for all books,user uploaded books,user bought books

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -38,7 +38,39 @@ exports.getHomeBookController = async (req,res)=>{
 }
 
 //get userallBookspage books - authorised user
+exports.getUserAllBooksController = async (req,res)=>{
+    console.log("Inside getUserAllBooksController");
+    const loginUserMail = req.payload
+    try{
+        const  allBooks = await books.find({sellerMail:{$ne:loginUserMail}})
+        res.status(200).json(allBooks)
+    }catch(error){
+        console.log(error);
+        res.status(500).json(error)        
+    }
+}
 
 //get userupload books- authorised user
-
+exports.getUserProfileBooksController = async (req,res)=>{
+    console.log("Inside getUserProfileBooksController");
+    const loginUserMail = req.payload
+    try{
+        const  userBooks = await books.find({sellerMail:loginUserMail})
+        res.status(200).json(userBooks)
+    }catch(error){
+        console.log(error);
+        res.status(500).json(error)        
+    }
+}
 // get user bought books- authorised user
+exports.getUserBoughtBooksController = async (req,res)=>{
+    console.log("Inside getUserBoughtBooksController");
+    const loginUserMail = req.payload
+    try{
+        const  userBooks = await books.find({buyerMail:loginUserMail})
+        res.status(200).json(userBooks)
+    }catch(error){
+        console.log(error);
+        res.status(500).json(error)        
+    }
+}
--- routes/router.js
+++ routes/router.js
@@ -12,7 +12,7 @@ router.post('/register',userController.registerController)
 router.post('/login',userController.loginController)
 //google login
 router.post('/google-login',userController.googleLoginController)
-//home projects
+//home books
 router.get('/home/books',bookController.getHomeBookController)
 
 
@@ -21,5 +21,11 @@ router.get('/home/books',bookController.getHomeBookController)
 
 //add book - request body in  formdata, header should has token
 router.post('/user/add/book',jwtMiddleware,multerMiddleware.array('uploadImg',3),bookController.addBookController)
+//all books
+router.get('/all-books',jwtMiddleware,bookController.getUserAllBooksController)
+//user books
+router.get('/user-books',jwtMiddleware,bookController.getUserProfileBooksController)
+//bought books
+router.get('/user-books/bought',jwtMiddleware,bookController.getUserBoughtBooksController)
 
 module.exports = router
