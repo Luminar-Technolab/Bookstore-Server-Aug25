7e87f152584ccba5a8b76b7e08af81081c5f1fe4
soniagkalliath <soniageorge111@gmail.com>
2025-12-18 15:41:15 +0530

    ADD : search book

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -40,9 +40,12 @@ exports.getHomeBookController = async (req,res)=>{
 //get userallBookspage books - authorised user
 exports.getUserAllBooksController = async (req,res)=>{
     console.log("Inside getUserAllBooksController");
+    const searchKey = req.query.search
+    console.log(searchKey);
+    
     const loginUserMail = req.payload
     try{
-        const  allBooks = await books.find({sellerMail:{$ne:loginUserMail}})
+        const  allBooks = await books.find({sellerMail:{$ne:loginUserMail},title:{$regex:searchKey,$options:'i'}})
         res.status(200).json(allBooks)
     }catch(error){
         console.log(error);
